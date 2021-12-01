1f8fac540347ea9bd0947ab1cb8108fb55cd2135
Salih Demirbas <salih.demirbas@testinium.com>
2021-12-01 10:03:21 +0300

    login succes

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -2,9 +2,15 @@ package org.example;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     WebDriver driver;
@@ -19,7 +25,17 @@ public class BaseTest {
         driver.get("https://www.gittigidiyor.com/"); //gideceğimiz adresi verdik..
     }
     @Test
-    public void ilk(){
-        System.out.println("deneme");
+    public void login() throws InterruptedException {
+
+        TimeUnit.SECONDS.sleep(5);
+        //atlamak icin yonlendirme yaptık. sonra tekrar bakmalıyız
+        driver.navigate().to("https://www.gittigidiyor.com/uye-girisi?s=1");
+        //email girisi yaptık
+        driver.findElement(By.id("L-UserNameField")).sendKeys("selenium_deneme@gmail.com");
+        //araya zaman/bekleme koydum yoksa API hatası veriyordu...
+        TimeUnit.SECONDS.sleep(5);
+        //sifre girisi yaptık ve enterladık
+        driver.findElement(By.id("L-PasswordField")).sendKeys("seleniumDeneme1"+ Keys.ENTER);
+
     }
 }
