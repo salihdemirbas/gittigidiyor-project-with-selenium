7f22c98cbac2daf3c4bf098b2c3d9b2f0ef8b611
Salih Demirbas <salih.demirbas@testinium.com>
2021-12-02 16:08:49 +0300

    new tab opened and closed, success

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -17,8 +17,8 @@ public class BaseTest {
     WebDriver driver;
 
     @Before
-    public void setUp(){
-        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe"); //chromedriver yeri
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe"); //chromedriver yeri
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized"); //ekrani büyükttük
 
@@ -37,9 +37,10 @@ public class BaseTest {
         //araya zaman/bekleme koydum yoksa API hatası veriyordu...
         TimeUnit.SECONDS.sleep(5);
         //sifre girisi yaptık ve enterladık
-        driver.findElement(By.id("L-PasswordField")).sendKeys("seleniumDeneme1"+ Keys.ENTER);
+        driver.findElement(By.id("L-PasswordField")).sendKeys("seleniumDeneme1" + Keys.ENTER);
 
     }
+
     @Test
     public void search() throws InterruptedException {
         //search olarak searchBox'a bilgisayar yazdırıldı ve enterladık...
@@ -47,16 +48,16 @@ public class BaseTest {
         TimeUnit.SECONDS.sleep(2);
         driver.findElement(By.cssSelector("input[data-cy='header-search-input']")).sendKeys("bilgisayar" + Keys.ENTER);
         JavascriptExecutor js = (JavascriptExecutor) driver;
-       // js.executeScript("window.scrollBy(0,)", "");
+        // js.executeScript("window.scrollBy(0,)", "");
         js.executeScript("window.scrollTo(0,document.body.scrollHeight)"); // sayfanın altına indi
         Random rnd = new Random();
-        int fav1= 1+ rnd.nextInt(5);
-        int fav2= 6+ rnd.nextInt(5);
-        int fav3= 11+ rnd.nextInt(5);
-        int fav4= 16+ rnd.nextInt(5);
-       TimeUnit.SECONDS.sleep(1);
-       // js.executeScript("window.scrollTo(0,-2000)");
-       // js.executeScript("window.scrollTo(0,350)");
+        int fav1 = 1 + rnd.nextInt(5);
+        int fav2 = 6 + rnd.nextInt(5);
+        int fav3 = 11 + rnd.nextInt(5);
+        int fav4 = 16 + rnd.nextInt(5);
+        TimeUnit.SECONDS.sleep(1);
+        // js.executeScript("window.scrollTo(0,-2000)");
+        // js.executeScript("window.scrollTo(0,350)");
         driver.findElements(By.cssSelector("div[class='sc-1n49x8z-14 fIkZfb']")).get(0).click();
         TimeUnit.SECONDS.sleep(1);
         js.executeScript("window.scrollTo(0,350)");
@@ -68,7 +69,6 @@ public class BaseTest {
         js.executeScript("window.scrollTo(0,350)");
         driver.findElements(By.cssSelector("div[class='sc-1n49x8z-14 fIkZfb']")).get(3).click();
         TimeUnit.SECONDS.sleep(1);
-
 
 
         // fav className:  sc-1n49x8z-14 fIkZfb
@@ -90,6 +90,7 @@ public class BaseTest {
         TimeUnit.SECONDS.sleep(3);
         driver.navigate().to("https://www.gittigidiyor.com/");
     }
+
     @Test
     public void cantaArat() throws InterruptedException {
         anaSayfayaDon();
@@ -107,43 +108,48 @@ public class BaseTest {
         TimeUnit.SECONDS.sleep(3);
 
     }
+
     @Test
     public void goToCart() throws InterruptedException {
         cantaArat();
         /*JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollTo(0,-(document.body.scrollHeight)"); // sayfanın altına indi
        */// driver.findElement(By.className("sc-84am1q-0 sc-1r48nyr-0 WZTpV kEgEYI")).click();
-       //driver.findElement(By.className("gekhq4-6 ojrwK")).click();
-       // title=Sepetim
+        //driver.findElement(By.className("gekhq4-6 ojrwK")).click();
+        // title=Sepetim
         driver.navigate().to("https://www.gittigidiyor.com/sepetim");
-              //  driver.findElement(By.cssSelector("input[title='Sepetim']")).click();
+        //  driver.findElement(By.cssSelector("input[title='Sepetim']")).click();
 
 
     }
+
     @Test
     public void adetArttir() throws InterruptedException {
         goToCart();
-   driver.findElement(By.cssSelector("select[class='amount']")).click();
+        driver.findElement(By.cssSelector("select[class='amount']")).click();
 
         TimeUnit.SECONDS.sleep(2);
 
         driver.findElement(By.cssSelector("option[value='2']")).click();
         TimeUnit.SECONDS.sleep(2);
-       // select.selectByVisibleText("2");
+        // select.selectByVisibleText("2");
         TimeUnit.SECONDS.sleep(2); //5 sn bekleme süresi ayarladık.*/
     }
+
     @Test
     public void odemeyeGit() throws InterruptedException {
         adetArttir();
         //driver.findElement(By.className("gg-d-24 gg-ui-btn-primary gg-ui-btn-lg btn-pay")).click();
         driver.findElement(By.cssSelector("input[value='Alışverişi Tamamla']")).click();
     }
+
     @Test
     public void adresKaydet() throws InterruptedException {
         odemeyeGit();
         driver.findElement(By.cssSelector("button[value='Kaydet']")).click();
         TimeUnit.SECONDS.sleep(5);
     }
+
     @Test
     public void sepetiDuzenle() throws InterruptedException {
         adresKaydet();
@@ -157,29 +163,32 @@ public class BaseTest {
         TimeUnit.SECONDS.sleep(2);
         js.executeScript("window.scrollTo(0,400)");
         driver.findElements(By.cssSelector("i[class='gg-icon gg-icon-close']")).get(2).click();
-       // driver.findElements(By.cssSelector("a[class='gg-ui-btn-default btn-add-to-basket']")).get(1).click();
-      //  driver.findElements(By.className("gg-ui-btn-default btn-add-to-basket")).get(1).click();
+        // driver.findElements(By.cssSelector("a[class='gg-ui-btn-default btn-add-to-basket']")).get(1).click();
+        //  driver.findElements(By.className("gg-ui-btn-default btn-add-to-basket")).get(1).click();
 
 
     }
+
     @Test
     public void yeniSekme() throws InterruptedException {
         sepetiDuzenle();
         TimeUnit.SECONDS.sleep(2);
-        ((JavascriptExecutor)driver).executeScript("window.open()");
+        ((JavascriptExecutor) driver).executeScript("window.open()");
         ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
         driver.switchTo().window(tabs.get(1));
         driver.get("https://www.gittigidiyor.com/");
         TimeUnit.SECONDS.sleep(2);
         driver.get("https://www.gittigidiyor.com/cikis-yap");
         driver.switchTo().window(tabs.get(0));
-       // TimeUnit.SECONDS.sleep(2);
+        // TimeUnit.SECONDS.sleep(2);
 
     }
+
     @Test
     public void cikisYap() throws InterruptedException {
         //burada cikis yap için locator bulamadım :( o sebeple link yonlendirme verdim.
         yeniSekme();
+        driver.close();// sekme kapatıldı
 
         //yeni sekme kapat
         /*ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
@@ -187,8 +196,7 @@ public class BaseTest {
         ((JavascriptExecutor)driver).executeScript("window.close()");*/
         TimeUnit.SECONDS.sleep(3);
         //driver.close();
-        driver.quit();
-
+        driver.quit(); //islem sonlandirildi...
 
 
     }
